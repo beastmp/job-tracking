2a52917a4f5ef523e5459737594cca4f202c9f0a
beastmp <beastmpdevelopment@gmail.com>
2025-04-16 13:16:43 -0700

    Fix import path in Vercel adapter and add Procfile and Fly configuration

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 // It takes your Express app and adapts it to Vercel's serverless functions
 
 // Import the actual Express app
-const serverlessAdapter = require('./backend/serverless-adapter');
+const serverlessAdapter = require('../backend/serverless-adapter');
 
 // Export the handler for Vercel
 module.exports = serverlessAdapter;
